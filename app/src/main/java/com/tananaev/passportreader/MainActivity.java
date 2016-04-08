caa45110f7c81087f792c266a9c497f843d43c0b
Anton Tananaev <anton.tananaev@gmail.com>
2016-04-09 01:09:59 +1200

    Add initial scanning functionality

--- app/src/main/java/com/tananaev/passportreader/MainActivity.java
+++ app/src/main/java/com/tananaev/passportreader/MainActivity.java
@@ -1,11 +1,32 @@
+/*
+ * Copyright 2016 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.tananaev.passportreader;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceFragment;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,11 +37,24 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
-import java.text.DateFormat;
+import net.sf.scuba.smartcards.CardFileInputStream;
+import net.sf.scuba.smartcards.CardService;
+
+import org.jmrtd.BACKey;
+import org.jmrtd.BACKeySpec;
+import org.jmrtd.PassportService;
+import org.jmrtd.lds.COMFile;
+import org.jmrtd.lds.DG1File;
+import org.jmrtd.lds.LDS;
+import org.jmrtd.lds.SODFile;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Locale;
+import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,6 +111,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    EditText passportNumberView;
+    EditText expirationDateView;
+    EditText birthDateView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,9 +122,9 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        EditText passportNumberView = (EditText) findViewById(R.id.input_passport_number);
-        EditText expirationDateView = (EditText) findViewById(R.id.input_expiration_date);
-        EditText birthDateView = (EditText) findViewById(R.id.input_date_of_birth);
+        passportNumberView = (EditText) findViewById(R.id.input_passport_number);
+        expirationDateView = (EditText) findViewById(R.id.input_expiration_date);
+        birthDateView = (EditText) findViewById(R.id.input_date_of_birth);
 
         passportNumberView.setText(preferences.getString(KEY_PASSPORT_NUMBER, null));
         expirationDateView.setText(preferences.getString(KEY_EXPIRATION_DATE, null));
@@ -127,6 +165,111 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        NfcAdapter adapter = NfcAdapter.getDefaultAdapter(this);
+        Intent intent = new Intent(getApplicationContext(), this.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        String[][] filter = new String[][] { new String[] { "android.nfc.tech.IsoDep" } };
+        adapter.enableForegroundDispatch(this, pendingIntent, null, filter);
+    }
+
+    private static String convertDate(String input) {
+        if (input == null) {
+            return null;
+        }
+        try {
+            return new SimpleDateFormat("yyMMdd", Locale.US)
+                    .format(new SimpleDateFormat("yyyy-MM-dd", Locale.US).parse(input));
+        } catch (ParseException e) {
+            Log.w(MainActivity.class.getSimpleName(), e);
+            return null;
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            Tag tag = intent.getExtras().getParcelable(NfcAdapter.EXTRA_TAG);
+            if (Arrays.asList(tag.getTechList()).contains("android.nfc.tech.IsoDep")) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                String passportNumber = preferences.getString(KEY_PASSPORT_NUMBER, null);
+                String expirationDate = convertDate(preferences.getString(KEY_EXPIRATION_DATE, null));
+                String birthDate = convertDate(preferences.getString(KEY_BIRTH_DATE, null));
+                if (passportNumber != null && !passportNumber.isEmpty()
+                        && expirationDate != null && !expirationDate.isEmpty()
+                        && birthDate != null && !birthDate.isEmpty()) {
+                    BACKeySpec bacKey = new BACKey(passportNumber, expirationDate, birthDate);
+                    new ReadTask(IsoDep.get(tag), bacKey).execute();
+                } else {
+                    Snackbar.make(passportNumberView, R.string.error_input, Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private class ReadTask extends AsyncTask<Void, Void, Boolean> {
+
+        private IsoDep isoDep;
+        private BACKeySpec bacKey;
+
+        public ReadTask(IsoDep isoDep, BACKeySpec bacKey) {
+            this.isoDep = isoDep;
+            this.bacKey = bacKey;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+
+                CardService cardService = CardService.getInstance(isoDep);
+                cardService.open();
+
+                PassportService service = new PassportService(cardService);
+                service.open();
+
+                service.sendSelectApplet(false);
+
+                service.doBAC(bacKey);
+
+                LDS lds = new LDS();
+
+                Collection<Integer> dgNumbersAlreadyRead = new TreeSet<Integer>();
+
+                CardFileInputStream comIn = service.getInputStream(PassportService.EF_COM);
+                lds.add(PassportService.EF_COM, comIn, comIn.getLength());
+                COMFile comFile = lds.getCOMFile();
+
+                CardFileInputStream sodIn = service.getInputStream(PassportService.EF_SOD);
+                lds.add(PassportService.EF_SOD, sodIn, sodIn.getLength());
+                SODFile sodFile = lds.getSODFile();
+
+                CardFileInputStream dg1In = service.getInputStream(PassportService.EF_DG1);
+                lds.add(PassportService.EF_DG1, dg1In, dg1In.getLength());
+                DG1File dg1File = lds.getDG1File();
+                dgNumbersAlreadyRead.add(1);
+
+            } catch (Exception e) {
+                return false;
+
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                Snackbar.make(passportNumberView, R.string.error_read, Snackbar.LENGTH_SHORT).show();
+            } else {
+                Snackbar.make(passportNumberView, "success", Snackbar.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
 }
